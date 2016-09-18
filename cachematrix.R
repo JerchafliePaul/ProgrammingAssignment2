9ecb4e4e7a5b914976f6e3f7aba9be3e401bfcb8
JerchafliePaul <twinpower2222@gmail.com>
2016-09-17 23:38:15 -0400

    submitting assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Creates a matrix object capable of calculating and caching its inverse
+makeCacheMatrix <- function(x=matrix()) {
+    ## the inverse 
+    inv<<-NULL
+    ##the matrix  
+    mat<-x
+    
+    getMatrix<-function()mat
+    setMatrix<-function(x=matrix()){s
+        mat<<-x
+    }
+    getInv<-function()  inv
+    setInv<-function(c){
+        inv<<-c
+    }
+    list(getMatrix= getMatrix,
+         getInv=getInv,setInv=setInv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##Calculates,caches, and returns the inverse of the argument 
+cacheSolve <- function(x,...) {
+    i<-x$getInv()
+    if(!is.null(i)){
+        print("Reading cached data")
+        print(i)
+    }else{
+        data<-x$getMatrix()
+        
+        print("Calculating the inverse...")
+        inver<-solve(data)
+        
+        print("Caching the data")
+        x$setInv(inver)
+        
+        print("The inverse is ")
+        y<-x$getInv()
+        print(y)
+    }
+    
 }
+##Test cases
+testMatrix1<-matrix(rbind(1:2,2:1), nrow = 2, ncol = 2, byrow = TRUE)
+obj<-makeCacheMatrix(testMatrix1)
+print("The matrix is")
+testMatrix1
+cacheSolve(obj)
+cacheSolve(obj)
+
+testMatrix2<-matrix(rbind(1:3,c(0,1,4),c(5,6,0)),nrow=3,ncol=3,byrow=FALSE)
+obj<-makeCacheMatrix(testMatrix2)
+print("The matrix is")
+testMatrix2
+cacheSolve(obj)
+cacheSolve(obj)
+
+
+
+
+
+
+
